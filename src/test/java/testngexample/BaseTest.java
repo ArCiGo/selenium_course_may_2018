ada10062f273390f4e8f3122c99cf454e1d13445
Gaby <gmorenogaby@gmail.com>
2019-09-23 22:46:32 -0500

    feature-activity-testNG

---
+++ src/test/java/testngexample/BaseTest.java
@@ -0,0 +1,24 @@
+package testngexample;
+
+import com.qalabs.seleniumbasics.WebDriverFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest {
+    protected WebDriver myDriver;
+    @BeforeClass (alwaysRun= true)
+    public void setUp(){
+        //Define which browser to use
+        String browser = "chrome";
+
+        //Get correct driver for desire browser
+        myDriver = WebDriverFactory.getDriver(browser);
+
+    }
+    @AfterClass(alwaysRun = true)
+    public void tearDown(){
+        myDriver.close();
+        myDriver.close();
+    }
+}
---
+++ src/test/java/testngexample/TestNGExample1.java
@@ -0,0 +1,26 @@
+package testngexample;
+
+import com.qalabs.seleniumbasics.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNGExample1 {
+    @Test
+    public static void passTC() {
+        String browser = "chrome";
+        WebDriver testDriver = WebDriverFactory.getDriver(browser);
+        testDriver.manage().window().maximize();
+        testDriver.get("https://www.google.com.mx/");
+
+        testDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        Assert.assertEquals(testDriver.getCurrentUrl(), "https://www.google.com.mx/", "Parece que no es Google");
+
+        testDriver.close();
+
+        testDriver.quit();
+    }
+}
