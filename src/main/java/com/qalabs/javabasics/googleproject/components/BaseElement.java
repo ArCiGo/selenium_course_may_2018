7a8012e988b2c765256db9fa800cc6bafc457799
Angelina Guzman <gelo0412@gmail.com>
2019-09-27 16:45:00 -0500

    google testNG

---
+++ src/main/java/com/qalabs/javabasics/googleproject/components/BaseElement.java
@@ -0,0 +1,12 @@
+package com.qalabs.javabasics.googleproject.components;
+
+import org.openqa.selenium.WebElement;
+
+public class BaseElement {
+    public class BaseElement {
+        protected WebElement root;
+        //protected final static Logger logger = Logger.getLogger(BaseElement.class);
+        public  BaseElement(WebElement root) {
+            this.root = root;
+        }
+}
